865b02edede7821f3a44c58b76da743b56d7c433
Aetherian <saarinenotso@gmail.com>
2024-10-01 14:13:11 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import Ohjelma2 from "./Ohjelma2";
 import Ohjelma3 from "./Ohjelma3";
 import "./App.css";
 
+function Home() {
+    return <h1>Welcome to the home page</h1>;
+}
+
 function App() {
     return (
         <div>
             <nav>
-                <Link to="/Ohjelma1">Ohjelma 1</Link> |{" "}
-                <Link to="/Ohjelma2">Ohjelma 2</Link> |{" "}
+                <Link to="/">Home</Link> | <Link to="/Ohjelma1">Ohjelma 1</Link>{" "}
+                | <Link to="/Ohjelma2">Ohjelma 2</Link> |{" "}
                 <Link to="/Ohjelma3">Ohjelma 3</Link>
             </nav>
 
             <Routes>
+                <Route path="/" element={<Home />} />
                 <Route path="/Ohjelma1" element={<Ohjelma1 />} />
                 <Route path="/Ohjelma2" element={<Ohjelma2 />} />
                 <Route path="/Ohjelma3" element={<Ohjelma3 />} />
