f5c832b3ab7ac13982fd76b2cd4dbea2e8b27149
MiskaTup <83165152+MiskaTup@users.noreply.github.com>
2024-11-12 19:12:11 +0200

    rename

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Link } from "react-router-dom";
-import Ohjelma1 from "./Ohjelma1";
-import Ohjelma2 from "./Ohjelma2";
-import Ohjelma3 from "./Ohjelma3";
+import Veriarvot from "./Veriarvot";
+import Potilastiedot from "./Potilastiedot";
+import Chattibotti from "./Chattibotti";
 import "./App.css";
 
 function Home() {
@@ -12,16 +12,16 @@ function App() {
     return (
         <div className="center">
             <nav>
-                <Link to="/">Home</Link> <Link to="/Ohjelma1">Veriarvot</Link>{" "}
-                <Link to="/Ohjelma2">Potilastiedot</Link>{" "}
-                <Link to="/Ohjelma3">Chattibotti</Link>
+                <Link to="/">Home</Link> <Link to="/Veriarvot">Veriarvot</Link>{" "}
+                <Link to="/Potilastiedot">Potilastiedot</Link>{" "}
+                <Link to="/Chattibotti">Chattibotti</Link>
             </nav>
 
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/Ohjelma1" element={<Ohjelma1 />} />
-                <Route path="/Ohjelma2" element={<Ohjelma2 />} />
-                <Route path="/Ohjelma3" element={<Ohjelma3 />} />
+                <Route path="/Veriarvot" element={<Veriarvot />} />
+                <Route path="/Potilastiedot" element={<Potilastiedot />} />
+                <Route path="/Chattibotti" element={<Chattibotti />} />
             </Routes>
         </div>
     );
