c330c54e756d524985e5f63e5490d6715c046bee
MiskaTup <83165152+MiskaTup@users.noreply.github.com>
2024-11-22 10:58:54 +0200

    Potilastiedot

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,6 @@ function App() {
             </Routes>
         </div>
     );
-}
+npm}
 
 export default App;
--- src/Potilastiedot.jsx
+++ src/Potilastiedot.jsx
@@ -1,9 +1,75 @@
-function Ohjelma2() {
+import { DataGrid } from "@mui/x-data-grid";
+import Box from "@mui/material/Box";
+
+const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    { field: "Käynti", headerName: "Käynti", width: 450 },
+  
+    {
+        field: "Päivämäärä",
+        headerName: "Päivämäärä",
+        type: "number",
+        width: 140,
+        editable: true,
+    },
+  
+];
+
+const rows = [
+    {
+        id: 1,
+        Käynti: "Vastaanotto", 
+        Päivämäärä: "10.07.2024",
+    },
+    {
+        id:2,
+        Käynti: "Hammaslääkäri",
+        Päivämäärä: "10.06.2023"
+        
+    }
+   
+];
+
+function Ohjelma1() {
     return (
-        <div>
-            <h1>Tämä on ohjelman 2 sivu</h1>
-        </div>
+        <>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "flex-start",
+                    height: "100vh",
+                    textAlign: "center",
+                }}
+            >
+                <h1>Veriarvot</h1>
+
+                <Box
+                    sx={{
+                        height: 400,
+                        width: "80%",
+                        backgroundColor: "white",
+                    }}
+                >
+                    <DataGrid
+                        rows={rows}
+                        columns={columns}
+                        initialState={{
+                            pagination: {
+                                paginationModel: {
+                                    pageSize: 5,
+                                },
+                            },
+                        }}
+                        pageSizeOptions={[5]}
+                        checkboxSelection
+                        disableRowSelectionOnClick
+                    />
+                </Box>
+            </div>
+        </>
     );
 }
 
-export default Ohjelma2;
+export default Ohjelma1;
